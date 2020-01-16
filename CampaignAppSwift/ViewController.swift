3213c3cf748d43a297bf6d1725778c4af1820213
Kevin Marcelino <kevinmarcelino@Kevins-MacBook-Pro.local>
2020-01-16 11:33:33 +0700

    Connected image url from firebase to swift

--- CampaignAppSwift/ViewController.swift
+++ CampaignAppSwift/ViewController.swift
@@ -11,10 +11,12 @@ import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var image: UIImageView!
     
     @IBOutlet weak var descriptor: UILabel!
     var refProfile: FirebaseApp!
     var ref: DatabaseReference!
+    var url="https://campaigndata-campaign.appspot.com/?t=upd&w=500&crop=true&file="
     override func viewDidLoad() {
         super.viewDidLoad()
         FirebaseApp.configure()
@@ -25,12 +27,30 @@ class ViewController: UIViewController {
             let value = Snapshot.value as? NSDictionary
             let username = value?["firstname"] as? String ?? ""
             let descriptionfinal=value?["descrip"] as? String ?? ""
+            let imageUrl=value?["avatar"] as? String ?? ""
             self.Name.text=username
             self.Name.numberOfLines=6
     self.descriptor.text=descriptionfinal
+            print(imageUrl)
             print(descriptionfinal)
             print(username)
+            let urlImage=URL(string:self.url+imageUrl)
+            self.image.load(url: urlImage!)
             })
 }
 
+  
 }
+extension UIImageView {
+      func load(url: URL) {
+          DispatchQueue.global().async { [weak self] in
+              if let data = try? Data(contentsOf: url) {
+                  if let image = UIImage(data: data) {
+                      DispatchQueue.main.async {
+                          self?.image = image
+                      }
+                  }
+              }
+          }
+      }
+    }
