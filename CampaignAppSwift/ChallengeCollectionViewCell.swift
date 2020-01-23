05bdb015f254d9ff3c2f967214b3ed70bea55642
Kevin Marcelino <kevinmarcelino@Kevins-MacBook-Pro.local>
2020-01-23 15:37:54 +0700

    Cells now show up in the eventChallenges page and can be coloured cyan

---
+++ CampaignAppSwift/ChallengeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChallengeCollectionViewCell.swift
+//  CampaignAppSwift
+//
+//  Created by Kevin Marcelino on 23/01/20.
+//  Copyright © 2020 Kevin Marcelino. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var challengeTitle: UILabel!
+}
--- CampaignAppSwift/EventChallengesViewController.swift
+++ CampaignAppSwift/EventChallengesViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class EventChallengesViewController: UIViewController {
-   
+class EventChallengesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+   var items = ["1", "2", "3", "4", "5", "6"]
+    let reuseIdentifier = "cell"
     @IBOutlet weak var eventImage: UIImageView!
     @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var challengeCollectionView: UICollectionView!
     var eventName = ""
     var eventImageUrl = ""
     var value = "es"
     @IBOutlet weak var eventDescripLabel: UILabel!
     var eventDescrip = ""
+  
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.eventNameLabel.text = self.eventName
+      
+
         let url = URL(string: self.eventImageUrl)
         self.eventImage.load(url: url!)
         self.eventDescripLabel.text=self.eventDescrip
@@ -28,7 +35,22 @@ class EventChallengesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
 
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ChallengeCollectionViewCell
+
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.challengeTitle.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+
+        return cell
+    }
 
     /*
     // MARK: - Navigation
