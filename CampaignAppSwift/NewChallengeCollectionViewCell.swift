baf56525af8a8b9835971c10c6ec3101bc6614c8
Kevin Marcelino <kevinmarcelino@Kevins-MacBook-Pro.local>
2020-01-23 12:56:59 +0700

    Able to scroll in homepage and there are no issues inserting collectionviewTable to eventChallengesPage

---
+++ CampaignAppSwift/NewChallengeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewChallengeCollectionViewCell.swift
+//  CampaignAppSwift
+//
+//  Created by Kevin Marcelino on 22/01/20.
+//  Copyright © 2020 Kevin Marcelino. All rights reserved.
+//
+
+import UIKit
+
+class NewChallengeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
